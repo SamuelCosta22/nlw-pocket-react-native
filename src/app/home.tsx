95003d56a9cbca0e8020962de9bb8f1555d8292d
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2024-12-12 15:21:39 -0300

    feat: add categories menu

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -2,14 +2,17 @@ import { Alert, Text, View } from "react-native";
 
 import { api } from "@/services/api";
 import { useEffect, useState } from "react";
+import { Categories, CategoriasProps } from "@/components/categories";
 
 export default function Home() {
-  const [categories, setCategories] = useState();
+  const [categories, setCategories] = useState<CategoriasProps>([]);
+  const [category, setCategory] = useState("");
 
   async function fetchCategories() {
     try {
       const { data } = await api.get("/categories");
       setCategories(data);
+      setCategory(data[0].id);
     } catch (error) {
       console.log(error);
       Alert.alert("Categorias", "Não foi possível carregar as categorias.");
@@ -22,7 +25,7 @@ export default function Home() {
 
   return (
     <View style={{ flex: 1 }}>
-      <Text>Home</Text>
+      <Categories data={categories} onSelect={setCategory} selected={category} />
     </View>
   );
 }
